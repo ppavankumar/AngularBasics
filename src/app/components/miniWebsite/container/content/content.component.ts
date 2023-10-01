d0e323338e367b7661612770dcb3f02456026f58
Pavan Kumar <pavankumar.pisipati@gmail.com>
2023-10-02 00:24:00 +0530

    Data binding using method in expression

--- src/app/components/miniWebsite/container/content/content.component.ts
+++ src/app/components/miniWebsite/container/content/content.component.ts
@@ -7,10 +7,20 @@ import { Component } from '@angular/core';
 })
 export class ContentComponent {
   time = new Date();
+  counter: number = 1;
+  slogan: string = 'Your one stop for everything. ';
 
   constructor() {
     setInterval(() => {
       this.time = new Date();
     }, 1000);
+  }
+
+  incrementAndGetChangedSlogan = () => {
+    return ++this.counter;
+  }
+
+  getSlogan = () => {
+    return this.slogan +  'Value now: ' + this.counter;
   }
 }
--- src/app/components/miniWebsite/container/notification/notification.component.ts
+++ src/app/components/miniWebsite/container/notification/notification.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-notification',
-  template: `<div class="notif-div">
+  template: `<div class="alert alert-success">
                 <p>This website uses cookies to provide better user experience.</p>
              </div>`,
   styles: [".notif-div{margin: 5px 0; padding: 2px 10px; background-color: #fad7a0; text-align: center;}", "p{font-size: 14px;}"]
