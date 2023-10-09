6f6bc0b6b9ba7150d7969c1e049893acaa3aa039
Pavan Kumar <pavankumar.pisipati@gmail.com>
2023-10-09 19:48:21 +0530

    Resetted code and now added template reference demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,36 +2,11 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
-import { ContainerComponent } from "./components/miniWebsite/container/container.component";
-import { NavbarComponent } from "./components/miniWebsite/container/navbar/navbar.component";
-import { ContentComponent } from "./components/miniWebsite/container/content/content.component";
-import { NotificationComponent } from "./components/miniWebsite/container/notification/notification.component";
-import { SearchComponent } from "./components/miniWebsite/container/search/search.component";
 import { FormsModule } from "@angular/forms";
-import { ProductsComponent } from "./components/miniWebsite/container/products/products.component";
-import { CoursesComponent } from "./components/miniWebsite/courses/courses.component";
-import { HeaderComponent } from "./components/miniWebsite/courses/header/header.component";
-import { CoursesSearchComponent } from "./components/miniWebsite/courses/search/search.component";
-import { CoursesFilterComponent } from "./components/miniWebsite/courses/filter/filter.component";
-import { ColorFillComponent } from "./components/miniWebsite/color-fill/color-fill.component";
-import { ColorValueComponent } from "./components/miniWebsite/color-fill/color-value/color-value.component";
+import { DemoComponent } from './components/demo/demo.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ContainerComponent,
-    NavbarComponent,
-    ContentComponent,
-    NotificationComponent,
-    SearchComponent,
-    ProductsComponent,
-    CoursesComponent,
-    HeaderComponent,
-    CoursesSearchComponent,
-    CoursesFilterComponent,
-    ColorFillComponent,
-    ColorValueComponent,
-  ],
+  declarations: [AppComponent, DemoComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/demo/demo.component.spec.ts
+++ src/app/components/demo/demo.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ContentComponent } from './content.component';
+import { DemoComponent } from './demo.component';
 
-describe('ContentComponent', () => {
-  let component: ContentComponent;
-  let fixture: ComponentFixture<ContentComponent>;
+describe('DemoComponent', () => {
+  let component: DemoComponent;
+  let fixture: ComponentFixture<DemoComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ContentComponent ]
+      declarations: [ DemoComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(ContentComponent);
+    fixture = TestBed.createComponent(DemoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
