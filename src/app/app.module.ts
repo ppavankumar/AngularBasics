7b3a09d378b4d8a06c917365b0e135e3f3362278
Pavan Kumar <pavankumar.pisipati@gmail.com>
2023-10-10 18:41:10 +0530

    Learnt about @HostListener which is used for Event Handling.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { FormsModule } from "@angular/forms";
 import { DemoComponent } from "./components/demo/demo.component";
 import { SetBackgroundDirective } from "./customDirectives/setBackground.directive";
 import { HighlightDirective } from './customDirectives/highlight.directive';
+import { HoverDirective } from './customDirectives/hover.directive';
 
 @NgModule({
-  declarations: [AppComponent, DemoComponent, SetBackgroundDirective, HighlightDirective],
+  declarations: [AppComponent, DemoComponent, SetBackgroundDirective, HighlightDirective, HoverDirective],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
