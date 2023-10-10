2dacf72c071f4d04c15bf8612b92e76417624922
Pavan Kumar <pavankumar.pisipati@gmail.com>
2023-10-10 08:20:53 +0530

    Template reference example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 import { FormsModule } from "@angular/forms";
 import { DemoComponent } from './components/demo/demo.component';
+import { CustomerListComponent } from './components/customer-list/customer-list.component';
 
 @NgModule({
-  declarations: [AppComponent, DemoComponent],
+  declarations: [AppComponent, DemoComponent, CustomerListComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/demo/demo.component.ts
+++ src/app/components/demo/demo.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-demo',
-  templateUrl: './demo.component.html',
-  styleUrls: ['./demo.component.css']
+  selector: "app-demo",
+  templateUrl: "./demo.component.html",
+  styleUrls: ["./demo.component.css"],
 })
 export class DemoComponent {
-
+  getMyVariableValue = (myVariable: HTMLInputElement) => {
+    alert("Hello, " + myVariable.value);
+  };
 }
