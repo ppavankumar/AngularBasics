451ecc717783eac34d4f103ee1445a522db034e1
Pavan Kumar <pavankumar.pisipati@gmail.com>
2023-10-10 13:31:40 +0530

    ng-content learning & demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,10 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
 import { FormsModule } from "@angular/forms";
-import { Comp1Component } from './components/comp1/comp1.component';
-import { Comp2Component } from './components/comp2/comp2.component';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
-  declarations: [AppComponent, Comp1Component, Comp2Component],
+  declarations: [AppComponent, ProductComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
