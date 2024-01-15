690c755b7cc60874f7aeeb3eb062df52814eeeab
Pavan Kumar <pavankumar.pisipati@gmail.com>
2024-01-15 07:51:01 +0530

    Cleaning main branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { PersonComponentComponent } from "./person-component/person-component.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [FormsModule, CommonModule, RouterOutlet, PersonComponentComponent]
+    imports: [FormsModule, CommonModule, RouterOutlet]
 })
 export class AppComponent {
   title = 'my-sample-angular-project';
