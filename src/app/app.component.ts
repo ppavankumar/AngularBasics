385b292bd1eafb4221792e201d372d8011b4627e
Pavan Kumar <pavankumar.pisipati@gmail.com>
2024-01-15 08:39:14 +0530

    Heroes angular tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, UpperCasePipe } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [FormsModule, CommonModule, RouterOutlet]
+    imports: [FormsModule, CommonModule, RouterOutlet, HeroesComponent]
 })
 export class AppComponent {
-  title = 'my-sample-angular-project';
+  title = 'Tour of Heroes';
 }
